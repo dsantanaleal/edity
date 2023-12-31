4d8d62b5383476769af81fe0d7a79b8f138e108c
Diego Santana Leal <dsantanaleal@gmail.com>
2023-12-31 09:08:56 -0500

    Initial Commit

---
+++ simple_editor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  simple_editor
+//
+//  Created by Diego Alexander Santana Leal on 31/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ simple_editor/simple_editorApp.swift
@@ -0,0 +1,17 @@
+//
+//  simple_editorApp.swift
+//  simple_editor
+//
+//  Created by Diego Alexander Santana Leal on 31/12/23.
+//
+
+import SwiftUI
+
+@main
+struct simple_editorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
